23a65b0ddab68490b9d02c62f95bcd34edc0bf6e
Alvaro Crespo <alvaro.crespo02@estudiant.upf.edu>
2024-07-07 14:21:29 +0200

    Updated to newest PyTorch version :)

--- train.py
+++ train.py
@@ -1,13 +1,25 @@
 import pytorch_lightning as pl
 import argparse
 
-from model import PedalNet
+from model2 import PedalNet
 
 
 def main(args):
-    model = PedalNet(vars(args))
+    model = PedalNet(
+        num_channels=args.num_channels,
+        dilation_depth=args.dilation_depth,
+        num_repeat=args.num_repeat,
+        kernel_size=args.kernel_size,
+        batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        data=args.data,
+    )
+    print(args.gpus)
     trainer = pl.Trainer(
-        max_epochs=args.max_epochs, gpus=args.gpus, row_log_interval=100
+        max_epochs=args.max_epochs, 
+        accelerator="gpu" if args.gpus else "cpu",
+        devices=int(args.gpus) if args.gpus else 1,
+        log_every_n_steps=10
     )
     trainer.fit(model)
 
@@ -23,7 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("--learning_rate", type=float, default=3e-3)
 
     parser.add_argument("--max_epochs", type=int, default=1_500)
-    parser.add_argument("--gpus", default="0")
+    parser.add_argument("--gpus", default=None)
 
     parser.add_argument("--data", default="data.pickle")
     args = parser.parse_args()
